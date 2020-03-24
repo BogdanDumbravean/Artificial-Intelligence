ea80a4548745f9c4a15c3b95703dc2908e9bfad7
Dumbravean Bogdan <41543827+BogdanDumbravean@users.noreply.github.com>
2020-03-24 14:35:59 +0200

    Update Main.py

--- Lab 2/Main.py
+++ Lab 2/Main.py
@@ -5,6 +5,10 @@ Created on Tue Mar  3 14:10:53 2020
 @author: Bogdan
 """
 
+'''
+Queen's problem on nxn matrix
+'''
+
 from UI import UI
 from Controller import Controller
 from Problem import Problem
